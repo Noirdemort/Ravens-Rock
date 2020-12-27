f5a86079f04b4733cfcb26ba1db359563633f83e
Noirdemort <darkStillwater@protonmail.com>
2020-12-27 08:08:15 +0530

    Initial Commit

---
+++ Ravens Rock/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Ravens Rock
+//
+//  Created by Noirdemort on 27/12/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ravens Rock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ravens Rock
+//
+//  Created by Noirdemort on 27/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
